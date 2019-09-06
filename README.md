e6f3038c18df284bb2f63acee4ae71fdebc583cc
liyang <1909457483@qq.com>
2019-09-06 23:20:33 +0800

    新增 init 命令，用于初始化配置文件

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# deploy-cli
+# deploy-this
 自动化发布工具
+
+```bash
+# 安装
+$ yarn global add deploy-this # 或者 npm install deploy-this -g
+
+# 初始化配置文件
+$ deploy init
+
+# 发布项目
+$ deploy publish dev -m "v1.0.0 发布信息"
+```
--- bin/cli.js
+++ bin/cli.js
@@ -7,23 +7,37 @@ program
   .version(require("../package.json").version)
   .usage("<command> [options] 自动化发布项目"); //-h 打印的用户提示
 
-let modeValue = ''
-let messageValue = ''
+let commandValue = "";
+let modeValue = "";
+let messageValue = "";
+let isForce = false;
 
-program.option("-m, --message <message>", "执行 git commit 时的提交信息")
+program.option("-m, --message <message>", "执行 git commit 时的提交信息");
+program.option("-f, --force", "命令是否强制执行");
 
 program
-  .command('publish <mode>')
-  .description('发布项目，根据配置的模式')
-  .action(function (mode, options) {
-    modeValue = mode
-  })
+  .command("init")
+  .description("初始化发布配置")
+  .action(function() {
+    commandValue = "init";
+  });
 
+program
+  .command("publish <mode>")
+  .alias("p")
+  .description("发布项目，可选择配置的模式")
+  .action(function(mode, options) {
+    commandValue = "publish";
+    modeValue = mode;
+  });
 
 program.parse(process.argv);
 
-messageValue = program.message
+messageValue = program.message;
+isForce = program.force;
 
-if (modeValue) {
-  deploy.publish(modeValue, messageValue)
+if (commandValue === "init") {
+  deploy.init(isForce);
+} else if (commandValue === "publish") {
+  deploy.publish(modeValue, messageValue);
 }
