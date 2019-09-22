51637da34ded4ea0a4a8d2ebe6017d60936626ba
liyang <1909457483@qq.com>
2019-09-23 00:03:47 +0800

    feature: 将命令行参数传递给钩子函数

--- .deploy/config.js
+++ .deploy/config.js
@@ -1,10 +1,13 @@
+// const path = require("path");
+// const afterDeployJsDir = path.resolve(__dirname, "afterDeploy.js");
+
 module.exports = {
   base: {
     releaseRepo: "../deploy-cli-release",
     copyFiles: "dist/*",
-    beforeDeploy: 'npm run build',
-    afterDeploy: function (shell, configs) {
-      // shell.echo('执行 afterDeploy 成功')
+    beforeDeploy: "npm run build",
+    afterDeploy: function(shell, configs, params) {
+      // shell.exec(`node ${afterDeployJsDir} ${configs.branch} ${params.commitMsg}`);
     }
   },
   dev: {
